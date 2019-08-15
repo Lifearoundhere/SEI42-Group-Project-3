2100594382d0ab5aef9485c9567a1f102bec2830
Daniel H Cohen <hofcohen@gmail.com>
2019-08-15 15:37:05 +0100

    Seeds are ready

---
+++ db/data/dishData.js
@@ -0,0 +1,38 @@
+module.exports = [{
+  name: 'Burger with fries',
+  nativeName: 'המבורגר',
+  price: 5,
+  latitude: 51.515794,
+  longitude: -0.073482,
+  cuisineType: 'American',
+  tags: [ 'Deliciuse', 'Great extras' ],
+  rating: 4
+}, {
+  name: 'Falafel',
+  nativeName: 'פלאפל',
+  price: 3.50,
+  latitude: 51.515442,
+  longitude: -0.073185,
+  cuisineType: 'Egypt',
+  tags: [ 'Cheap', 'Great value for money' ],
+  rating: 3
+}, {
+  name: 'Dumplings',
+  nativeName: '饺',
+  price: 4.50,
+  latitude: 51.515842,
+  longitude: -0.073575,
+  cuisineType: 'China',
+  tags: [ 'Oriental', 'Real taste from the east' ],
+  comments: ['You can get a large one in £5.50', 'I tried it last week for the first time, and I loved it!'],
+  rating: 5
+}, {
+  name: 'Chicken Katsu wrap',
+  nativeName: 'チキンカツ',
+  price: 4.50,
+  latitude: 51.515825,
+  longitude: -0.073649,
+  cuisineType: 'Japan',
+  tags: [ 'Tasty', 'Just like a Shnizchel' ],
+  rating: 2
+}]
