ac6ea3c0ff9c4eca28409871909c5ef4b24b4607
Dmitrii <dmitriisolomatin@icloud.com>
2019-08-15 15:27:29 +0100

    add routes update include update for dishes.js... Commit comments creat & delete, in routes.js add const authControllers & dishesControllers

--- config/routes.js
+++ config/routes.js
@@ -1,22 +1,31 @@
 const router = require('express').Router()
+const dishesController = require('../controllers/dishes')
+const authController = require('../controllers/auth')
+// const secureRoute = require('../lib/secureRoute')
+// Don't forget to add...
 
-// defining the get request for homepage
+
+
+// Defining the get request for homepage
 router.get('/', (req, res) => {
   res.json({message: 'Hi Everybody'})
 })
 
-//Dishes index and show
+// Dishes index and show
+// Need to add secureRoute feature to create
 router.route('/dishes')
   .get(dishesController.index)
   .post(dishesController.create)
 
+// Dishes SHOW, UPDATE & DELETE
+// Need to add secureRoute features to update and delete
 router.route('/dishes/:id')
   .get(dishesController.show)
   .put(dishesController.update)
   .delete(dishesController.delete)
 
-
-router.post('/register', authCountroller.register)
+// REGISTER & LOGIN
+router.post('/register', authController.register)
 router.post('/login', authController.login)
 
 module.exports = router
