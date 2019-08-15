a3c9d29f426fcb0de26be7f6bf6e10dd65869969
Prab <lifearoundpss@gmail.com>
2019-08-15 15:26:11 +0100

    nav setup done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Navbar from './components/common/Navbar'
+
+import 'bulma'
+import './style.scss'
 
 class App extends React.Component {
-  render (){
-    return(
-      <h1>Hello World</h1>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>Hello World</h1>
+
+      </div>
     )
   }
 }
