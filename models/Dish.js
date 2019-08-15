b00d9e1de5779039d604e706de54e2bfb2d77694
Daniel H Cohen <hofcohen@gmail.com>
2019-08-15 14:28:02 +0100

    Dish Schema

---
+++ models/Dish.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const requireInSchema = 'Please provide a {PATH} of your dish.'
+
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: 'Please tell us a bit more.', minlength: 20}
+})
+
+
+const dishSchema = new mongoose.Schema({
+  name: { type: String, required: requireInSchema},
+  nativeName: {type: String},
+  price: {type: Number, required: requireInSchema},
+  latitude: { type: Number, required: requireInSchema },
+  longitude: { type: Number, required: requireInSchema },
+  cuisineType: {type: String, required: requireInSchema },
+  tags: [ String ],
+  comments: [ commentSchema ],
+  rating: { type: Number, min: 1, max: 5, required: requireInSchema}
+})
+
+module.exports = mongoose.model('Dish', dishSchema)
