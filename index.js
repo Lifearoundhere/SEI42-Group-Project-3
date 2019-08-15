37ecdb09a8544809513df1c223fb397aa9e4a4a6
Prab <lifearoundpss@gmail.com>
2019-08-15 16:36:36 +0100

    working backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.plugin(require('mongoose-unique-validator'), {
+  message: 'Please choose another {PATH}'
+})
+const router = require('./config/routes')
+const { dbURI } = require('./config/environment')
+
+const app = express()
+
+mongoose.connect(dbURI, { useNewUrlParser: true })
+
+app.use(express.json())
+
+app.use('/api', router)
+
+app.listen(4000, () => console.log('Mind the gap on port 4000'))
+
+module.exports = app
