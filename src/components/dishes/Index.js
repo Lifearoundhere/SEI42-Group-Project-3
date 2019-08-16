d4ea443f2bdc439898a222423a1e06e43759a294
Prab <lifearoundpss@gmail.com>
2019-08-16 14:55:35 +0100

    index fleshed out

--- src/components/dishes/Index.js
+++ src/components/dishes/Index.js
@@ -22,20 +22,103 @@ class Index extends React.Component {
     return (
       <section className="section">
         <div className="container">
-          <div className="columns">
+          <div className="columns is-multiline is-full-desktop">
             {!this.state.dishes && < h2 className="title is-2"> Loading...</h2>}
-
-            {this.state.dishes.map(dish =>
-              <div className="columns" key={dish._id}>
-                <Link to={`/dishes/${dish._id}`}>
-                  <div className="card">
-                    <div className="card-header">
-                      <h2 className="card-header-title">{dish.name}</h2>
+            <div className="column is-full-desktop is-two-thirds-tablet">
+              <div className="columns">
+                <div className="column">
+                  <div className="field">
+                    <label className="label">Search</label>
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Ethiopian" />
                     </div>
                   </div>
-                </Link>
+                </div>
+                <div className="column">
+                  <div className="field">
+                    <label className="label">Dietary</label>
+                    <div className="control">
+                      <div className="select is-fullwidth">
+                        <select>
+                          <option>None</option>
+                          <option>Gluten/Coeliac</option>
+                          <option>Dairy/lactose</option>
+                          <option>Vegetarian</option>
+                          <option>Vegan</option>
+                          <option>Nut Allergies</option>
+                          <option>Fish and shellfish</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="column">
+                  <div className="field">
+                    <label className="label">Cuisine</label>
+                    <div className="control">
+                      <div className="select is-fullwidth">
+                        <select>
+                          <option>None</option>
+                          <option>American</option>
+                          <option>Cajun</option>
+                          <option>Caribbean</option>
+                          <option>Chinese</option>
+                          <option>French</option>
+                          <option>German</option>
+                          <option>Greek</option>
+                          <option>Indian</option>
+                          <option>Italian</option>
+                          <option>Japanese</option>
+                          <option>Korean</option>
+                          <option>Lebanese</option>
+                          <option>Mediterranean</option>
+                          <option>Mexican</option>
+                          <option>Moroccan</option>
+                          <option>Soul</option>
+                          <option>Spanish</option>
+                          <option>Thai</option>
+                          <option>Turkish</option>
+                          <option>Vietnamese</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="column">
+                  <div className="field">
+                    <label className="label">Sort</label>
+                    <div className="control">
+                      <div className="select is-fullwidth">
+                        <select>
+                          <option>Price|High-Low</option>
+                          <option>Price|Low-High</option>
+                          <option>Rating|High-Low</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            )}
+            </div>
+
+
+
+
+            <div className="column">
+              {this.state.dishes.map(dish =>
+                <div className="columns" key={dish._id}>
+                  <Link to={`/dishes/${dish._id}`}>
+                    <div className="card">
+                      <div className="card-header">
+                        <h2 className="card-header-title">{dish.name}</h2>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              )}
+
+            </div>
           </div>
         </div>
       </section>
