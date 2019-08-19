bc2b04bfeb5699cf56559a9446f2a7351ed23d77
HerrChick <elliott.chick@gmail.com>
2019-08-19 10:54:55 +0100

    Setting up app for deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ mongoose.plugin(require('mongoose-unique-validator'), {
   message: 'Please choose another {PATH}'
 })
 const router = require('./config/routes')
-const { dbURI } = require('./config/environment')
+const { port, dbURI } = require('./config/environment')
 
 const app = express()
 
@@ -12,8 +12,13 @@ mongoose.connect(dbURI, { useNewUrlParser: true })
 
 app.use(express.json())
 
+//look for static files in the 'dist' folder
+//static files are like files in index.html, images, fonts, styles, etc.
+app.use(express.static(`${__dirname}/dist`))
+
 app.use('/api', router)
 
-app.listen(4000, () => console.log('Mind the gap on port 4000'))
+
+app.listen(port, () => console.log('Mind the gap on port 4000'))
 
 module.exports = app
