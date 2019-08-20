4327640ec3371223c8a1204cbfae62c430669750
HerrChick <elliott.chick@gmail.com>
2019-08-20 14:13:13 +0100

    minor changes

--- db/data/dishData.js
+++ db/data/dishData.js
@@ -16,6 +16,16 @@ module.exports = [{
       fullness: 3,
       healthiness: 5
     }]
+  },
+  {
+    user: 'test2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    mostHelpful: 3,
+    ratings: [{
+      overall: 1,
+      fullness: 2,
+      healthiness: 4
+    }]
   }]
 }, {
   name: 'Falafel',
